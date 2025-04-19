c1b8bbe38a0553de181938372646cfab5e93585e
Hammad Afzal <hammad.afzal.code@gmail.com>
2025-04-19 12:26:42 +0500

    added rate limiter and proxy

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -11,19 +11,33 @@ const app = express();
 app.use(
   cors({
     origin: ['http://localhost:3000'],
-    allowHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
+
 app.use(morgan('dev'));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
 app.set('trust proxy', 1);
-app.get('/api', (req, res) => {
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
 
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: 'Too many requests, please try again later.' },
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any) => req.ip,
+});
+
+app.use(limiter);
+
+app.use('/', proxy('http://localhost:6001'));
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
