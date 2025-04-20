b15695daf849b499f4f29acb67ad1ec4a9f0bee8
Hammad Afzal <hammad.afzal.code@gmail.com>
2025-04-20 13:51:40 +0500

    added prisma installation

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 6001;
@@ -14,7 +16,9 @@ app.use(
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
-
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(cookieParser());
 const server = app.listen(port, () => {
   console.log(`Auth service is running at http://localhost:${port}/api`);
 });
